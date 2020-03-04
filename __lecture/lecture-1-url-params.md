67444fc4793eff2b2a29a23a7a800dda7fc82ec8
Paul Susset <paul.susset@gmail.com>
2020-03-04 15:18:07 -0500

    passable I hope

--- __lecture/lecture-1-url-params.md
+++ __lecture/lecture-1-url-params.md
@@ -10,23 +10,29 @@ How do you feel about this? Is this DRY?
 // ...
 const app = express();
 
-app.get('/question1', q1)
-app.get('/question2', q2)
-app.get('/question3', q3)
-app.get('/question4', q4)
-app.get('/question5', q5)
-app.get('/question6', q6)
-app.get('/question7', q7)
-app.get('/question8', q8)
-app.get('/question9', q9)
-app.get('/question10', q10)
+app.get("/question1", q1);
+app.get("/question2", q2);
+app.get("/question3", q3);
+app.get("/question4", q4);
+app.get("/question5", q5);
+app.get("/question6", q6);
+app.get("/question7", q7);
+app.get("/question8", q8);
+app.get("/question9", q9);
+app.get("/question10", q10);
 ```
 
 </div><div>
 
 ```js
 // ...
-
+// www.../question/6
+app.get("/question/:number", (req, res) => {
+    const number = req.params.number;
+    console.log(number); // 6
+    exercisesP1[`q${number}`]();
+});
+// element after the colon is a variable that gets added to the params object. you can call it after, they name is only a key and not the value itself
 ```
 
 </div></div>
--- server.js
+++ server.js
@@ -1,27 +1,65 @@
-'use strict';
+"use strict";
 
-const morgan = require('morgan');
+const morgan = require("morgan");
+const express = require("express");
 
-const { top50 } = require('./data/top50');
+const { top50 } = require("./data/top50");
+const { books } = require("./data/books")
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 // endpoints here
-
+function booksHandle(req, res) {
+    let title = '25 books to read';
+    res.render("pages/books", {title:title, books:books})
+}
+function bookInfo(req, res){
+    const number = req.params.number
+    let book = books.find(book=> {
+        return book.id.toString() === number;});
+    
+    const title = book.title;
+    res.render("pages/bookPage", {title: title, book: book})
+}
+function bookTypeHandle(req, res){
+    const genre = req.params.genre
+    let title = genre;
+    let typeArr = books.filter(book=> book.type === genre);
+    res.render("pages/bookType", {title: title, typeArr: typeArr})
+}
 // handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
+app.get("/books", booksHandle)
+app.get("/books/:number", bookInfo)
+app.get("/books/type/:genre", bookTypeHandle)
+app.get("/top50", (req, res) => {
+    let title = "Top 50 Songs Streamed on Spotify";
+    res.render("pages/top50", { title: title, top50: top50 });
+});
+app.get("/top50/top-artist", (req, res) => {
+    let title = "Most popular artist";
+    res.render("pages/topArtist", { title: title, top50: top50 });
+});
+app.get("/top50/song/:rank", (req, res) => {
+    const rank = req.params.rank;
+    if (rank <=50 && rank>=1){
+    let title = `Song #${rank}`;
+    res.render("pages/song", { title: title, top50: top50, rankNum: rank });
+    } else {res.status(404);
+
+        res.render("pages/fourOhFour", { title: "Oops" });}
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get("*", (req, res) => {
+    res.status(404);
+
+    res.render("pages/fourOhFour", { title: "Oops" });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
